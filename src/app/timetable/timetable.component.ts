7d4278685330249d3d9a73d69f29bbb52e5063c5
William Chang <wilicw1346@gmail.com>
2019-03-10 12:02:51 +0800

    change timetable text

--- src/app/timetable/timetable.component.ts
+++ src/app/timetable/timetable.component.ts
@@ -9,10 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class TimetableComponent implements OnInit {
   display: string[] = ['no', 'mon', 'tue', 'wed', 'thu', 'fri'];
   DayTable = [
-    {no: 1, mon: '體育', tue: '英文', wed: '資訊', thu: '國文', fri: '選修'},
+    {no: 1, mon: '體育', tue: '英文', wed: '資訊', thu: '國文', fri: '家政'},
     {no: 2, mon: '美術', tue: '化學', wed: '資訊', thu: '地理', fri: '公民'},
     {no: 3, mon: '數學', tue: '國文', wed: '數學', thu: '專題', fri: '公民'},
-    {no: 4, mon: '英文', tue: '選修', wed: '化學', thu: '專題', fri: '歷史'},
+    {no: 4, mon: '英文', tue: '輔導', wed: '化學', thu: '專題', fri: '歷史'},
     {no: null, mon: '午休', tue: '午休', wed: '午休', thu: '午休', fri: '午休'},
     {no: 5, mon: '國文', tue: '數學', wed: '英文', thu: '體育', fri: '國文'},
     {no: 6, mon: '地科', tue: '音樂', wed: '地科', thu: '數學', fri: '班會'},
