5b202813a68eebf79c3d156fc544f4203007fc16
William Chang <wilicw1346@gmail.com>
2019-03-10 09:25:36 +0800

    add timetable 104

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
-import { MatCardModule, MatButtonModule, MatListModule, MatToolbarModule, MatTabsModule} from '@angular/material';
+import { MatCardModule, MatButtonModule, MatListModule, MatToolbarModule, MatTabsModule, MatTableModule} from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GoBusComponent } from './go-bus/go-bus.component';
 import { BackBusComponent } from './back-bus/back-bus.component';
 import { environment } from '../environments/environment';
+import { TimetableComponent } from './timetable/timetable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GoBusComponent,
-    BackBusComponent
+    BackBusComponent,
+    TimetableComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +27,7 @@ import { environment } from '../environments/environment';
     MatListModule,
     MatTabsModule,
     HttpClientModule,
+    MatTableModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
---
+++ src/app/timetable/timetable.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable',
+  templateUrl: './timetable.component.html',
+  styleUrls: ['./timetable.component.css']
+})
+
+export class TimetableComponent implements OnInit {
+  display: string[] = ['no', 'mon', 'tue', 'wed', 'thu', 'fri'];
+  DayTable = [
+    {no: 1, mon: '體育', tue: '英文', wed: '資訊', thu: '國文', fri: '選修'},
+    {no: 2, mon: '美術', tue: '化學', wed: '資訊', thu: '地理', fri: '公民'},
+    {no: 3, mon: '數學', tue: '國文', wed: '數學', thu: '專題', fri: '公民'},
+    {no: 4, mon: '英文', tue: '選修', wed: '化學', thu: '專題', fri: '歷史'},
+    {no: null, mon: '午休', tue: '午休', wed: '午休', thu: '午休', fri: '午休'},
+    {no: 5, mon: '國文', tue: '數學', wed: '英文', thu: '體育', fri: '國文'},
+    {no: 6, mon: '地科', tue: '音樂', wed: '地科', thu: '數學', fri: '班會'},
+    {no: 7, mon: '國防', tue: '歷史', wed: '地理', thu: '英文', fri: '綜合活動'},
+    {no: 8, mon: '地科', tue: '英文', wed: '歷史', thu: '數學', fri: null},
+    {no: 9, mon: '國文', tue: null, wed: null, thu: null, fri: null},
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
