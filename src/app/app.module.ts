a8074b8c97e459a9aae66ae04a92a3a829498c11
William Chang <wilicw1346@gmail.com>
2019-03-31 17:46:56 +0800

    add search bus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
-import { MatCardModule, MatButtonModule, MatListModule, MatToolbarModule, MatTabsModule, MatTableModule} from '@angular/material';
+import { MatGridListModule, MatInputModule, MatCardModule, MatButtonModule, MatListModule, MatToolbarModule, MatTabsModule, MatTableModule} from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,6 +30,8 @@ import { SearchComponent } from './search/search.component';
     MatTabsModule,
     HttpClientModule,
     MatTableModule,
+    MatInputModule,
+    MatGridListModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GetAuthorizationHeader} from '../allbus'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  num = '652'
+
+  showgo = []
+  showback = []
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  onKey(event: any) {
+    this.num = event.target.value;
+  }
+
+  search() {
+    let tpurl = `https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei?$filter=RouteName/Zh_tw eq '${this.num}'&$format=JSON`
+    let ntpurl = `https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/NewTaipei?$filter=RouteName/Zh_tw eq '${this.num}'&$format=JSON`
+    this.showgo = []
+    this.showback = []
+    let self = this
+    this.http.get(tpurl, {headers: GetAuthorizationHeader()}).subscribe((resp)=>{
+      if (resp.length == 0) {
+        this.http.get(ntpurl, {headers: GetAuthorizationHeader()}).subscribe((resp)=>{
+          if (resp.length == 0) {
+            alert("沒這路公車")
+          } else {
+            resp.forEach((item) => {
+              if (item.Direction == 1) {
+                self.showgo.push(`  ${item.StopName.Zh_tw} - ${Math.round(item.EstimateTime/60)} 分鐘`)
+              } else {
+                self.showback.push(`  ${item.StopName.Zh_tw} - ${Math.round(item.EstimateTime/60)} 分鐘`)
+              }
+            })
+          }
+        })
+      } else {
+        resp.forEach((item) => {
+          if (item.Direction == 1) {
+            self.showgo.push(`  ${item.StopName.Zh_tw} - ${Math.round(item.EstimateTime/60)} 分鐘`)
+          } else {
+            self.showback.push(`  ${item.StopName.Zh_tw} - ${Math.round(item.EstimateTime/60)} 分鐘`)
+          }
+        })
+      }
+    })
+
   }
 
 }
